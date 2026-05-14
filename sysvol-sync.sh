b2e80c3d413408f2dffaab35c2cfe00558f2850f
kags2020 <kags@kinghorndesign.com>
2026-05-14 23:37:21 +0100

    Generalize SYSVOL sync script

--- sysvol-sync.sh
+++ sysvol-sync.sh
@@ -2,25 +2,25 @@
 set -euo pipefail
 
 LOG="/var/log/sysvol-sync.log"
-SRC="/var/lib/samba/sysvol/ecs.kinghorndesign.com/"
-KEY="/home/kags/.ssh/id_ed25519_sysvol_sync"
+SRC="/var/lib/samba/sysvol/mybranch.mycompany.com/"
+KEY="/home/happychappie/.ssh/id_ed25519_sysvol_sync"
 
 {
   echo "===== SYSVOL sync started: $(date -Is) ====="
 
   rsync -aHAX --delete -e "ssh -i ${KEY}" \
     "${SRC}" \
-    kags@ad2:/tmp/sysvol-ecs.kinghorndesign.com/
+    happychappie@ad2:/tmp/sysvol-mybranch.mycompany.com/
 
-  ssh -i "${KEY}" kags@ad2 \
-    "sudo rsync -aHAX --delete /tmp/sysvol-ecs.kinghorndesign.com/ /var/lib/samba/sysvol/ecs.kinghorndesign.com/"
+  ssh -i "${KEY}" happychappie@ad2 \
+    "sudo rsync -aHAX --delete /tmp/sysvol-mybranch.mycompany.com/ /var/lib/samba/sysvol/mybranch.mycompany.com/"
 
   rsync -aHAX --delete -e "ssh -i ${KEY}" \
     "${SRC}" \
-    kags@ad3:/tmp/sysvol-ecs.kinghorndesign.com/
+    happychappie@ad3:/tmp/sysvol-mybranch.mycompany.com/
 
-  ssh -i "${KEY}" kags@ad3 \
-    "sudo rsync -aHAX --delete /tmp/sysvol-ecs.kinghorndesign.com/ /var/lib/samba/sysvol/ecs.kinghorndesign.com/"
+  ssh -i "${KEY}" happychappie@ad3 \
+    "sudo rsync -aHAX --delete /tmp/sysvol-mybranch.mycompany.com/ /var/lib/samba/sysvol/mybranch.mycompany.com/"
 
   echo "===== SYSVOL sync completed: $(date -Is) ====="
   echo
